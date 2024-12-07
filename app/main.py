f30a83522b9cad7b531ea03f8bee86aa8eaf0f75
Muhammad Badawy <32201952+Badawekoo@users.noreply.github.com>
2024-12-08 00:29:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,37 @@
 from typing import Union
-
+import databases
 from fastapi import FastAPI
+
+# Load environment variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", 5432)
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Construct the database URL
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Initialize the database connection
+database = databases.Database(DATABASE_URL)
 
 app = FastAPI()
 
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World from Main Release"}
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World from Main Release"}
+async def read_root():
+    return {"message": "Connected to Postgres!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
