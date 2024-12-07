b87d2d40fc3b56d7a40f3c82d1a8d7d955b8b0e6
Muhammad Badawy <32201952+Badawekoo@users.noreply.github.com>
2024-12-07 23:41:23 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World from Main"}
+    return {"Hello": "World from Main Release"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
